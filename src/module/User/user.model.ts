cfd331f4895a6a2eebca4f16d128836faef7be87
mominulislamsharon <ilsamikchannel328250@gmail.com>
2024-12-19 05:56:34 +0600

    user interface and model

---
+++ src/module/User/user.model.ts
@@ -0,0 +1,35 @@
+import { model, Schema } from 'mongoose';
+import { IUser } from './user.interface';
+
+const userSchema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Please enter a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const User = model<IUser>('User', userSchema);
